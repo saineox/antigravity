7f24cdffef5ca7194a1e04185d969fe28f56b495
Pradeep Kadam <pradeeptraje@gmail.com>
2026-05-20 15:51:22 +0530

    Fresh clean commit

---
+++ newstart.bat
@@ -0,0 +1,7 @@
+# reinitialize
+git init
+git add .
+git commit -m "Fresh clean commit"
+git branch -M main
+git remote add origin https://github.com/saineox/antigravity.git
+git push -f origin main
